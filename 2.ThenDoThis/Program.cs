15be2cf526bdb8c85b8a95cce3555459129bfe2a
Nickpodski <Nickpodski@gmail.com>
2023-07-17 16:34:44 -0400

    Finished 2.ThenDoThis

--- 2.ThenDoThis/Program.cs
+++ 2.ThenDoThis/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace _2.Puzzle.Medium
 {
@@ -57,14 +60,14 @@ namespace _2.Puzzle.Medium
 
             foreach (var list in Output(Resource.SimpleList))
             {
-                Console.WriteLine(string.Join(",", list));
+                Console.WriteLine(string.Join(", ", list));
             }
 
             Console.WriteLine("\r\n\r\nSimpleList complete.\r\n");
 
             foreach (var list in Output(Resource.HarderList))
             {
-                Console.WriteLine(string.Join(",", list));
+                Console.WriteLine(string.Join(", ", list));
             }
 
             Console.WriteLine("\r\n\r\nHarderList complete.\r\n\r\n");
@@ -74,10 +77,72 @@ namespace _2.Puzzle.Medium
         static IEnumerable<IEnumerable<string>> Output(IEnumerable<string> input)
         {
             var output = new List<List<string>>();
+            var inputList = input.Select(s => string.IsNullOrWhiteSpace(s) ? null : s.Trim()).OrderBy(s => s).ToList();
 
-            // YOUR CODE GOES HERE
+            do
+            {
+                if (inputList[0] != null)
+                {
+                    var currentWord = inputList[0].ToString();
+                    IEnumerable<string> newList;
+                    inputList.Remove(currentWord);
+                    var tempList = inputList;
+                    if (currentWord != "")
+                    {
+                        newList = new[] { currentWord };
+                        for (var j = 0; j < tempList.Count(); j++)
+                        {
+                            var tempWord = tempList[j];
+                            if (tempWord != null)
+                            {
+                                if (IsAnagram(currentWord, tempWord))
+                                {
+                                    newList = newList.Concat(new[] { tempWord });
+                                    inputList.Remove(tempWord);
+                                    tempList.Remove(tempWord);
+                                }
+                            }
+                        }
+                        output.Add(newList.Distinct().ToList());
+                    }
+                }
+                else
+                {
+                    inputList.Remove(inputList[0]);
+                }
+            } while (inputList.Count() > 0);
 
             return output;
         }
+
+        static bool IsAnagram(string stringOne, string stringTwo) 
+        {
+            stringOne = RemoveWhiteSpace(stringOne);
+            stringTwo = RemoveWhiteSpace(stringTwo);
+
+            if (stringOne.Length != stringTwo.Length)
+            {
+                return false;
+            }
+
+            var stringOneChars = stringOne.ToLower().ToCharArray();
+            var stringTwoChars = stringTwo.ToLower().ToCharArray();
+
+            Array.Sort(stringOneChars);
+            Array.Sort(stringTwoChars);
+
+            for (var i = 0; i < stringOneChars.Length; i++)
+            {
+                if (stringOneChars[i].ToString() != stringTwoChars[i].ToString()) return false;
+            }
+
+            return true;
+        }
+
+        public static string RemoveWhiteSpace(string input)
+        {
+            return new Regex(@"\s+").Replace(input, "");
+        }
+
     }
 }
